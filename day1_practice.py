3b1aa3bce6dd8481a47a1ad2d62bdb2a1192348e
FEBA ROY <sesa835988@se.com>
2026-05-19 19:12:34 +0530

    File based analytics

--- day1_practice.py
+++ day1_practice.py
@@ -2,24 +2,31 @@ name = input("Enter your name: ")
 expenses = []
 categories = []
 
-file = open("expenses.txt", "a")
+def update_expenses():
+    categories.clear()
+    expenses.clear()
+    with open("expenses.txt", "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            categories.append(line.split('|')[0].strip())
+            expenses.append(float(line.split('|')[1].strip()))
 
-def add_expenses(expenses, categories):
+def add_expenses():
     n = int(input("Enter the number of expenses you want to add: "))
     for i in range(n):
         expense = float(input(f"Enter your expense {i+1}: "))
         category = input(f"Enter the category of your expense {i+1}: ")
         if expense > 1000:
             print(f"{expense} is a high expense.")
-        expenses.append(expense)
-        categories.append(category)
-        file = open("expenses.txt", "a")
-        file.write(f"{categories[i]} | {expenses[i]}\n")
+        with open("expenses.txt", "a") as file:
+            file.write(f"{category} | {expense}\n")
+    update_expenses()
     print("Expense Recorded!")
 
-def show_expenses(file):
-    file = open("expenses.txt", "r")
-    print(file.read())
+def show_expenses():
+    with open("expenses.txt", "r") as file:
+        print(file.read())
+    file.close()
     
 def total_expenses(expenses):
     total = 0
@@ -39,12 +46,17 @@ def max_expense(expenses):
 
 def category_search(categories):
     target = input("Enter the category to search: ")
-    sum = 0
+    total = 0
     for i in range(len(categories)):
         if categories[i].lower() == target.lower():
             print(f"{target} | {expenses[i]}")
-            sum += expenses[i]
-    print(f"Total {target} expenses: {sum}")
+            total += expenses[i]
+    print(f"Total {target} expenses: {total}")
+
+def delete_all_expenses():
+    with open("expenses.txt","w") as file:
+        file.write("")
+    print("All your expenses are deleted.")
 
 def menu(expenses, categories):
     print("------MENU------")
@@ -54,15 +66,16 @@ def menu(expenses, categories):
     print("4. Count Expenses")
     print("5. Max Expense")
     print("6. Category Search")
-    print("7. Exit")
+    print("7. Delete All Expenses")
+    print("8. Exit")
     
     option = int(input("Enter your choice: "))
     
     if option == 1:
-        add_expenses(expenses, categories)
+        add_expenses()
         
     elif option == 2:
-        show_expenses(file)
+        show_expenses()
     
     elif option == 3:
         print(f"Total Expenses: {total_expenses(expenses)}")
@@ -78,12 +91,14 @@ def menu(expenses, categories):
         category_search(categories)
     
     elif option == 7:
+        delete_all_expenses()
+        
+    elif option == 8:
         exit()
-    
+        
     else:
         print("Invalid choice. Try again.")
 
-file.close()
-
 while True:
+    update_expenses()
     menu(expenses, categories)
