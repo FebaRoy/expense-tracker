bc91c3b546fb5a656509b99ee2573767a0372dd3
FebaRoy <febaroy9@gamil.com>
2026-08-13 13:07:56 +0530

    Exception Handling

--- day1_practice.py
+++ day1_practice.py
@@ -7,33 +7,46 @@ categories = []
 def update_expenses():
     categories.clear()
     expenses.clear()
-    with open("expenses.txt", "r") as file:
-        lines = file.readlines()
-        for line in lines:
-            categories.append(line.split('|')[0].strip())
-            expenses.append(float(line.split('|')[1].strip()))
+    try: 
+        with open("expenses.txt", "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                categories.append(line.split('|')[0].strip())
+                expenses.append(float(line.split('|')[1].strip()))
+    except FileNotFoundError:
+        print("No expense file found. Starting refresh!")
 
 def add_expenses():
-    n = int(input("Enter the number of expenses you want to add: "))
+    n = valid_input("Enter the number of expenses you want to add: ")
     for i in range(n):
-        expense = float(input(f"Enter your expense {i+1}: "))
-        category = input(f"Enter the category of your expense {i+1}: ")
-        if expense > 0:
-            if expense > 1000:
-                print(f"{expense} is a high expense.")
-            with open("expenses.txt", "a") as file:
-                file.write(f"{category} | {expense}\n")
-                print("Expense Recorded!")
-        else:
-            print("Expense cannot be 0.")
+        try: 
+            expense = float(input(f"Enter your expense {i+1}: "))
+            category = input(f"Enter the category of your expense {i+1}: ")
+            if expense > 0:
+                if expense > 1000:
+                    print(f"{expense} is a high expense.")
+                try:
+                    with open("expenses.txt", "a") as file:
+                        file.write(f"{category} | {expense}\n")
+                        print("Expense Recorded!")
+                except FileNotFoundError:
+                    print("No expense file found. Starting refresh")
+            else:
+                print("Expense must be greater than 0.")
+        except ValueError:
+            print("Invalid input.")
     update_expenses() 
 
 def show_expenses():
-    with open("expenses.txt", "r") as file:
-        lines = file.readlines()
-        for line in lines:
-            print(f"Category: {line.split('|')[0].strip()}")
-            print(f"Expense: {line.split('|')[1].strip()}")
+    try:
+        with open("expenses.txt", "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                print(f"Category: {line.split('|')[0].strip()}")
+                print(f"Expense: {line.split('|')[1].strip()}")
+                print()
+    except FileNotFoundError:
+        print("No expense file found. Starting refresh!")
 
 def total_expenses(expenses):
     total = 0
@@ -64,8 +77,11 @@ def category_search(categories):
         print(f"Total {target} expenses: {total}")
 
 def delete_all_expenses():
-    with open("expenses.txt","w") as file:
-        file.write("")
+    try:
+        with open("expenses.txt","w") as file:
+            file.write("")
+    except FileNotFoundError:
+        print("No expense file found. Starting refresh!")
     print("All your expenses are deleted.")
 
 def clear_screen():
@@ -73,6 +89,16 @@ def clear_screen():
         os.system('cls')
     else:
         os.system('clear')
+
+def valid_input(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            return value
+        except ValueError:
+            print(f"Invalid input. {prompt}")
+            value = int(input(prompt))
+        
 
 def show_menu():
     print("------MENU------")
@@ -88,41 +114,40 @@ def show_menu():
 
 def menu(expenses, categories):
     show_menu()
-    
-    option = int(input("Enter your choice: "))
-    
+
+    option = valid_input("Enter your choice: ")
+
     if option == 1:
         add_expenses()
         
     elif option == 2:
         show_expenses()
-    
+        
     elif option == 3:
         print(f"Total Expenses: {total_expenses(expenses)}")
         print()
-    
+        
     elif option == 4:
         print(f"You recorded {count_expenses(expenses)} expenses.")
-    
+        
     elif option == 5:
         print(f"Highest Expense: {max_expense(expenses)}")
-    
+        
     elif option == 6: 
         category_search(categories)
-    
+        
     elif option == 7:
         delete_all_expenses()
-        
+            
     elif option == 8:
         exit()
 
     elif option == 9:
         clear_screen()
-        
+
     else:
         print("Invalid choice. Try again.")
-        menu(expenses, categories)
-
+        
 while True:
     update_expenses()
     menu(expenses, categories)
